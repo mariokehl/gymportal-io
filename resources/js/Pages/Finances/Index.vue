0b4c38c60c84bfb565c95dd65caf94babbf28cbd
Mario Kehl <bureau@mariokehl.com>
2025-07-02 17:25:01 +0200

    Show mollie setup again under finances

--- resources/js/Pages/Finances/Index.vue
+++ resources/js/Pages/Finances/Index.vue
@@ -3,9 +3,21 @@
         <template #header>
             Finanzen
         </template>
+        <MollieSetupWizard :organization="currentOrganization" />
     </AppLayout>
 </template>
 
 <script setup>
+import { computed } from 'vue'
+import { usePage } from '@inertiajs/vue3'
 import AppLayout from '@/Layouts/AppLayout.vue'
+import MollieSetupWizard from '@/Components/MollieSetupWizard.vue'
+
+// Shared data
+const page = usePage()
+
+// Computed properties
+const currentOrganization = computed(() => {
+    return page.props.auth.user.current_gym;
+})
 </script>
