4cb3646fa289ffe3d2358d1cf79c553ed813969a
Mario Kehl <bureau@mariokehl.com>
2025-07-04 15:56:13 +0200

    Revert "Update current_gym_in in migration instead of setting it in seed"
    This reverts commit 8c26fd83e19c4a744a17de9bc96a46603f3dd53c.

--- database/migrations/2025_06_23_064620_add_current_gym_to_users_table.php
+++ database/migrations/2025_06_23_064620_add_current_gym_to_users_table.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -16,14 +15,6 @@ return new class extends Migration
             $table->foreignId('current_gym_id')->nullable()->constrained('gyms')->onDelete('set null');
             $table->index(['current_gym_id']);
         });
-
-        $this->updateExistingUsers();
-    }
-
-    private function updateExistingUsers()
-    {
-        DB::table('users')->whereIn('email', ['max@fitzone.de', 'lisa@fitzone.de', 'thomas@fitzone.de'])->update(['current_gym_id' => 1]);
-        DB::table('users')->where('email', 'anna@powerfit.de')->update(['current_gym_id' => 2]);
     }
 
     /**
