c7630d25948c500f66dbfeff1cc7268d2cc3665b
Mario Kehl <bureau@mariokehl.com>
2025-08-30 10:38:35 +0200

    Add Chatwoot Integration

---
+++ config/chatwoot.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'website_token' => env('CHATWOOT_WEBSITE_TOKEN'),
+    'base_url' => env('CHATWOOT_BASE_URL', 'https://app.chatwoot.com'),
+    'enabled' => env('CHATWOOT_ENABLED', true),
+];
