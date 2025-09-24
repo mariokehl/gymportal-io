c399a0265bf14470bc1efce0533b14650f623de4
Mario Kehl <bureau@mariokehl.com>
2025-09-24 12:40:51 +0200

    Add all common German validation messages

--- resources/lang/de/validation.php
+++ resources/lang/de/validation.php
@@ -1,15 +1,22 @@
 <?php
 
 return [
-    'indisposable' => 'Wegwerf-E-Mail-Adressen sind nicht erlaubt.',
-    'confirmed' => 'Die Bestätigung für :attribute stimmt nicht überein.',
-    'attributes' => [
-        'password' => 'Passwort',
-        'email' => 'E-Mail-Adresse',
+    'required' => ':attribute ist erforderlich.',
+    'email' => ':attribute muss eine gültige E-Mail-Adresse sein.',
+    'min' => [
+        'string' => ':attribute muss mindestens :min Zeichen haben.',
     ],
-    'custom' => [
-        'password' => [
-            'confirmed' => 'Die Passwörter stimmen nicht überein.',
-        ],
+    'max' => [
+        'string' => ':attribute darf maximal :max Zeichen haben.',
+    ],
+    'confirmed' => 'Die :attribute-Bestätigung stimmt nicht überein.',
+    'unique' => ':attribute ist bereits vergeben.',
+    'indisposable' => 'Wegwerf-E-Mail-Adressen sind nicht erlaubt.',
+
+    'attributes' => [
+        'email' => 'E-Mail-Adresse',
+        'password' => 'Passwort',
+        'name' => 'Name',
+        'password_confirmation' => 'Passwort-Bestätigung',
     ],
 ];
